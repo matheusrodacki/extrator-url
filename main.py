efcb429f8c9956924aabb77dcf30495f6c21144d
matheusrodacki <44122620+matheusrodacki@users.noreply.github.com>
2023-06-22 09:38:38 +0000

    URI Sliced

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-url = 'https://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100'
+uri = 'https://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100'
+
+print(uri)
+
+urn = uri[8:27]
+url = uri[0:21]
+query = uri[28:80]
+
+print(urn)
 print(url)
+print(query)
