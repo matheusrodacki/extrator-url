c7b557da02384ff0b2d08c8f3bb9c8ecf31d7fc5
matheusrodacki <matheus.rodacki@outlook.com>
2023-06-26 21:33:10 -0300

    add parameter searsh

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-uri = 'https://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100'
+url = "bytebank.com/cambio?quantidade=100&moedaOrigem=real&moedaDestino=dolar"
+indice_interrogacao = url.find('?')
+url_base = url[:indice_interrogacao]
+url_parametros = url[indice_interrogacao+1:]
 
-print(uri)
+parametro_busca = 'moedaOrigem'
+indice_parametro = url_parametros.find(parametro_busca)
+indice_valor = indice_parametro + len(parametro_busca) + 1
 
-urn = uri[8:27]
-url = uri[0:21]
-query = uri[28:80]
-
-print(urn)
-print(url)
-print(query)
+indice_e_comercial = url_parametros.find('&', indice_valor)
+if indice_e_comercial == -1:
+    valor = url_parametros[indice_valor:]
+else:
+    valor = url_parametros[indice_valor:indice_e_comercial]
+print(valor)
